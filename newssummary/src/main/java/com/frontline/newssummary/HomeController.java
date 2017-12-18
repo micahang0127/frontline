44cfc5f04f9897a24b8cead419b66740570f4170
Administrator <Administrator@PAVIL-116>
2017-12-18 16:15:19 +0900

    12

--- newssummary/src/main/java/com/frontline/newssummary/HomeController.java
+++ newssummary/src/main/java/com/frontline/newssummary/HomeController.java
@@ -1,11 +1,13 @@
 package com.frontline.newssummary;
 
-import java.text.DateFormat;
-import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
 
+import org.apache.ibatis.session.SqlSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +19,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 	
+	@Autowired
+	private SqlSession sqlSession;
+	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	/**
@@ -24,14 +29,28 @@ public class HomeController {
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
+//		logger.info("Welcome home! The client locale is {}.", locale);
+//		
+//		Date date = new Date();
+//		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+//		
+//		String formattedDate = dateFormat.format(date);
+//		
+//		model.addAttribute("serverTime", formattedDate );
+//		
+//		// insert 구문
+//		HashMap<String, String> input = new HashMap<String, String>();
+//		input.put("keyword", "example1");
+//		input.put("description", "example2");
+//		sqlSession.insert("userControlMapper.exInsert", input);
+//
+//		// select 구문
+//		HashMap<String, String> input2 = new HashMap<String, String>();
+//		input2.put("keyword", "example1");
+//		List<HashMap<String, String>> output = sqlSession.selectList("userControlMapper.exSelect", input2);
+//		System.out.print(output);
+//		
+//		
 		
 		return "home";
 	}
--- newssummary/src/main/webapp/WEB-INF/views/Login.jsp
+++ newssummary/src/main/webapp/WEB-INF/views/Login.jsp
@@ -1,9 +1,9 @@
-<%@ page language="java" contentType="text/html; charset=EUC-KR"
-    pageEncoding="EUC-KR"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=EUC-KR">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <script type="text/javascript">
 	function focusIt() {
 		document.inform.id.focus();
@@ -12,12 +12,12 @@
 	function checkIt(){
 		inputForm=eval("document.inform");
 		if(!inputForm.id.value){
-			alert("¾ÆÀÌµğ¸¦ ÀÔ·ÂÇÏ¼¼¿ä~");
+			alert("ì•„ì´ë””ë¥¼ ì…ë ¥í•˜ì„¸ìš”~");
 			inputForm.id.focus();
 			return false;
 		}
 		if(!inputForm.password.value){
-			alert("ºñ¹Ğ¹øÈ£¸¦ ÀÔ·ÂÇÏ¼¼¿ä~");
+			alert("ë¹„ë°€ë²ˆí˜¸ë¥¼ ì…ë ¥í•˜ì„¸ìš”~");
 			inputForm.password.focus();
 			return false;
 		}
@@ -25,22 +25,22 @@
 
 
 </script>
-<title>·Î±×ÀÎ</title>
+<title>ë¡œê·¸ì¸</title>
 </head>
 
 <center>
 <body onload="focusIt()">
 	<form name="inform"  method="post"  action="-------------" onsubmit="return checkIt();">
-	<b><font size="5" color="skyblue">·Î±×ÀÎ</font></b><br/>
+	<b><font size="5" color="skyblue">ë¡œê·¸ì¸</font></b><br/>
 	<table border="3"  bordercolor="skyblue">
 		<tr>
 		<td bgcolor="skyblue"  style="text-align:center" >&nbsp; &nbsp; &nbsp; ID :&nbsp; &nbsp; &nbsp;     <input type="text"  name="id"  width="50'"/></td></tr>
 		<tr>
 		<td bgcolor="skyblue" >Password : <input type="password"  name="password" width="50'"/></td></tr>
 		</table><br/>
-		<input type="submit" value="·Î±×ÀÎ"/>
-	<input type="button" value="¾ÆÀÌµğ/ºñ¹Ğ¹øÈ£ Ã£±â"  onclick="location.href="findLogin.jsp"/>
-	<input type="button" value="È¸¿ø°¡ÀÔ"  onclick="location.href="join.jsp"/>
+		<input type="submit" value="ë¡œê·¸ì¸"/>
+	<input type="button" value="ì•„ì´ë””/ë¹„ë°€ë²ˆí˜¸ ì°¾ê¸°"  onclick="location.href="findLogin.jsp"/>
+	<input type="button" value="íšŒì›ê°€ì…"  onclick="location.href="join.jsp"/>
 
 </form>
 </body>
